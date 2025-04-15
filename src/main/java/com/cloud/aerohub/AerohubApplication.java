d2f9927ed0c8c487f7ef2d951d7462446155657f
I578055 <shekapuram.rutvik@sap.com>
2025-04-15 22:19:15 +0530

    Initial commit

---
+++ src/main/java/com/cloud/aerohub/AerohubApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.aerohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerohubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/aerohub/AerohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.aerohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
